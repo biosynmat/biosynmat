d4ff83dde7e4e41e36bbd9b8082c394452dcd85a
Mohammed Abid Nafi <abid.nafi04@gmail.com>
2026-03-16 04:46:12 +0530

    fix: footer

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -5,8 +5,8 @@ import { Building2, Linkedin, Mail } from "lucide-react";
 export function SiteFooter() {
   return (
     <footer className="border-t border-slate-200 bg-slate-50/80">
-      <div className="mx-auto grid w-full max-w-7xl gap-8 px-4 py-8 text-sm text-slate-600 sm:px-6 lg:grid-cols-[1.2fr_0.8fr] lg:px-8">
-        <div className="flex items-start">
+      <div className="mx-auto flex flex-col items-start md:flex-row! justify-between w-full max-w-7xl gap-8 px-4 py-8 text-sm text-slate-600 sm:px-6 lg:grid-cols-[1.2fr_0.8fr] lg:px-8">
+        <div className="flex items-center">
           <Link href="/" className="inline-flex items-center gap-3">
             <Image
               src="/logo.png"
@@ -15,10 +15,8 @@ export function SiteFooter() {
               height={160}
               className="h-16 w-16 rounded-md object-contain sm:h-24 sm:w-24 lg:h-32 lg:w-32"
             />
-            <div>
-              <p className="text-2xl font-semibold text-slate-900 sm:text-3xl lg:text-4xl">
-                BioSynMat
-              </p>
+            <div className="font-semibold text-base md:text-2xl!  tracking-[0.18em]">
+              <span>BIOSYNMAT</span>
             </div>
           </Link>
           {/* <p className="mt-2 max-w-md leading-relaxed">
@@ -27,8 +25,8 @@ export function SiteFooter() {
           </p> */}
         </div>
 
-        <div className="flex flex-col items-start lg:items-end">
-          <div className="mb-4 flex w-full max-w-xs flex-col">
+        <div className="flex flex-col items-center lg:items-end">
+          <div className="mb-4 flex  max-w-xs flex-col">
             <p className="mb-2 inline-flex items-center gap-1.5 text-xs font-semibold uppercase tracking-[0.1em] text-slate-500">
               <Building2 className="h-3.5 w-3.5" />
               Institution
@@ -38,14 +36,13 @@ export function SiteFooter() {
               target="_blank"
               rel="noopener noreferrer"
               aria-label="Visit SRM Institute of Science and Technology website"
-              className="inline-flex"
             >
               <Image
                 src="/srm.png"
                 alt="SRM Institute logo"
                 width={180}
                 height={64}
-                className="h-14 w-auto object-contain"
+                className="h-10 w-fit md:h-14 object-contain"
               />
             </a>
           </div>
